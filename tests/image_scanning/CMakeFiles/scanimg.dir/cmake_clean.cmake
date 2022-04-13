36ce544bffa898ff57e79dda2a9b0e881023006f
Pecneb <ecneb2000@gmail.com>
2022-04-13 21:42:27 +0200

    Color Space reduction

---
+++ tests/image_scanning/CMakeFiles/scanimg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scanimg.dir/scanimg.cpp.o"
+  "CMakeFiles/scanimg.dir/scanimg.cpp.o.d"
+  "scanimg"
+  "scanimg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scanimg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
